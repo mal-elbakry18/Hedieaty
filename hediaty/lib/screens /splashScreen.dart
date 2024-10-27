1fa43e3fea2a2f9179a3fcf1215ed74757c30dd2
mal-elbakry18 <100995605+mal-elbakry18@users.noreply.github.com>
2024-10-27 19:59:16 +0300

    2 pages done

---
+++ hediaty/lib/screens /splashScreen.dart
@@ -0,0 +1,39 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+//Simple entry page of just the title then routes to the app's home page
+//Using the splash screen
+
+class Splash_Front extends StatefulWidget {
+  const Splash_Front({super.key});
+
+  @override
+  _SplashFrontState createState() => _SplashFrontState();
+}
+
+class _SplashFrontState extends State<Splash_Front> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushReplacementNamed(context, '/Login');
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.teal,
+      body: Center(
+        child: Text(
+          "Hediety",
+          style: TextStyle(
+              fontSize: 90,
+              fontFamily: 'Anton',
+              fontWeight: FontWeight.bold,
+              color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
